054b8c6f5c65a67fb1e75e7fef46022039d2fe26
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-06-03 11:25:50 +0200

    fourth commit

--- src/main/java/com/example/ToDoList.java
+++ src/main/java/com/example/ToDoList.java
@@ -4,4 +4,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ToDoList{
-}
+
+
+    public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("\n--- To-Do List ---");
+            System.out.println("1. Add Task");
+            System.out.println("2. Delete Task");
+            System.out.println("3. View Tasks");
+            System.out.println("4. Mark Task as Done");
+            System.out.println("5. Exit");
+            System.out.print("Select an option: ");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            }
+        }
